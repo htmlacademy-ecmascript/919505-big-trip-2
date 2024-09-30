cb14ff2d576a698387ee6df351355060b32a4a22
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2024-09-30 20:28:58 +0300

    Добавляет отрисовку компонентов

---
+++ src/const.js
@@ -0,0 +1,6 @@
+export const RenderPosition = {
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
+};
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import {RenderPosition} from './const';
+import {render} from './render.js';
+import PointFilterView from './view/point-filter';
+import TripInfoView from './view/trip-info';
+import Board from './presenter/board';
+
+const headerElement = document.querySelector('.page-header');
+const tripInfoContainerElement = headerElement.querySelector('.trip-main');
+const FilterContainerElement = headerElement.querySelector('.trip-controls__filters');
+
+const boardContainer = document.querySelector('.trip-events');
+const boardPresenter = new Board(boardContainer);
+
+render(new TripInfoView(), tripInfoContainerElement, RenderPosition.AFTERBEGIN);
+render(new PointFilterView(), FilterContainerElement);
+
+boardPresenter.init();
