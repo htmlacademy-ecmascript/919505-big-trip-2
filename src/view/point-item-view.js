892da85f9f30d6ab189dee6a99b1c7cd5e32832f
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2024-10-04 15:58:01 +0300

    Добавляет отображение данных в point-item-view

--- src/view/point-item-view.js
+++ src/view/point-item-view.js
@@ -1,9 +1,20 @@
 import {createElement} from '../render';
 import {humanizePointDate, humanizePointHours, humanizeTimeDifference, DateFormat} from '../utils';
 
+function createOfferTemplate(offer) {
+  const {title, price} = offer;
+
+  return (
+    `<li class="event__offer">
+      <span class="event__offer-title">${title}</span>
+        &plus;&euro;&nbsp;
+      <span class="event__offer-price">${price}</span>
+    </li>`
+  );
+}
+
 function createPointItemTemplate(point) {
-  const {type, destination, dateFrom, dateTo} = point;
-  console.log(point);
+  const {type, destination, dateFrom, dateTo, basePrice, offers, isFavorite} = point;
 
   const pointFullDateFrom = humanizePointDate(dateFrom);
   const pointFullDateTo = humanizePointDate(dateFrom);
@@ -11,6 +22,10 @@ function createPointItemTemplate(point) {
   const timeFrom = humanizePointHours(dateFrom);
   const timeTo = humanizePointHours(dateTo);
   const duration = humanizeTimeDifference(dateFrom, dateTo);
+
+  const offersTemplate = offers.map((offer) => createOfferTemplate(offer)).join('');
+
+  const isPointFavoriteClassName = isFavorite ? 'event__favorite-btn--active' : '';
 
   return (
     `<li class="trip-events__item">
@@ -29,17 +44,13 @@ function createPointItemTemplate(point) {
           <p class="event__duration">${duration}</p>
         </div>
         <p class="event__price">
-          &euro;&nbsp;<span class="event__price-value">20</span>
+          &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
         </p>
         <h4 class="visually-hidden">Offers:</h4>
         <ul class="event__selected-offers">
-          <li class="event__offer">
-            <span class="event__offer-title">Order Uber</span>
-            &plus;&euro;&nbsp;
-            <span class="event__offer-price">20</span>
-          </li>
+          ${offersTemplate}
         </ul>
-        <button class="event__favorite-btn event__favorite-btn--active" type="button">
+        <button class="event__favorite-btn ${isPointFavoriteClassName}" type="button">
           <span class="visually-hidden">Add to favorite</span>
           <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
             <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
