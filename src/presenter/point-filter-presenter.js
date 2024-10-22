614d236b8497ba88987036a2b37e6aa5d3dd5da4
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2024-10-22 13:17:53 +0300

    Рефакторит модель и презентер фильтра

--- src/presenter/point-filter-presenter.js
+++ src/presenter/point-filter-presenter.js
@@ -1,11 +1,14 @@
 import {render, RenderPosition} from '../framework/render';
 import PointFilterView from '../view/point-filter-view';
+import {pointsFilter} from '../utils/filter';
 
 export default class PointFilterPresenter {
   #pointFilterContainerElement = null;
   #pointsModel = null;
   #filterModel = null;
   #currentFilter = null;
+
+  #filters = null;
 
   constructor({pointFilterContainerElement, pointsModel, filterModel}) {
     this.#pointFilterContainerElement = pointFilterContainerElement;
@@ -15,8 +18,13 @@ export default class PointFilterPresenter {
 
   init() {
     this.#currentFilter = this.#filterModel.currentFilter;
-    const filters = this.#filterModel.getFilters(this.#pointsModel.points);
+    this.#filters = Object.entries(pointsFilter).map(
+      ([filterType, filterPoints]) => ({
+        type: filterType,
+        count: filterPoints(this.#pointsModel.points).length,
+      }),
+    );
 
-    render(new PointFilterView({filters, currentFilter: this.#currentFilter}), this.#pointFilterContainerElement, RenderPosition.AFTERBEGIN);
+    render(new PointFilterView({filters: this.#filters, currentFilter: this.#currentFilter}), this.#pointFilterContainerElement, RenderPosition.AFTERBEGIN);
   }
 }
