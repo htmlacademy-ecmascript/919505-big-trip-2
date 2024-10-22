704b55d38beddeb7ecb9ff0fbe79bcc91fca59a1
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2024-10-22 10:12:09 +0300

    Рефакторит trip-info-view

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -2,24 +2,26 @@ import AbstractView from '../framework/view/abstract-view.js';
 import {humanizeTripTimeInterval} from '../utils/dates';
 import {generateTitle, generateTripValue} from '../utils/trip-info';
 
-function createCitiesAndDatesTemplate(points, destinations) {
-  const title = generateTitle(points, destinations);
+const MAIN_TITLE_PLACEHOLDER = 'Big Trip';
+
+function createDatesTemplate(points) {
   const timeInterval = humanizeTripTimeInterval(points);
 
   return (
-    `<div class="trip-info__main">
-      <h1 class="trip-info__title">${title}</h1>
-      <p class="trip-info__dates">${timeInterval}</p>
-    </div>`
+    `<p class="trip-info__dates">${timeInterval}</p>`
   );
 }
 
 function createTripInfoTemplate(points, destinations, offers) {
+  const title = points.length > 0 ? generateTitle(points, destinations) : MAIN_TITLE_PLACEHOLDER;
   const tripValue = generateTripValue(points, offers);
 
   return (
     `<section class="trip-main__trip-info  trip-info">
-      ${points.length > 0 ? createCitiesAndDatesTemplate(points, destinations) : ''}
+      <div class="trip-info__main">
+        <h1 class="trip-info__title">${title}</h1>
+        ${points.length > 0 ? createDatesTemplate(points) : ''}
+      </div>
       <p class="trip-info__cost">
         Total: &euro;&nbsp;<span class="trip-info__cost-value">${tripValue}</span>
       </p>
