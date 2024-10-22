a6448590452a16d78e98ed65af5ce7b2fc35c8b0
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2024-10-22 10:44:25 +0300

    Рефакторит point-sorting-panel-view

--- src/view/point-sorting-panel-view.js
+++ src/view/point-sorting-panel-view.js
@@ -3,18 +3,18 @@ import {SortType} from '../const';
 
 const DISABLED_SORT_TYPES = [SortType.EVENT, SortType.OFFER];
 
-function createSortItemTemplate (currentSortType, sortingType) {
+function createSortItemTemplate (currentSortType, sortType) {
   let isDisabled = '';
-  const isChecked = sortingType === currentSortType ? 'checked' : '';
+  const isChecked = sortType === currentSortType ? 'checked' : '';
 
-  if (DISABLED_SORT_TYPES.includes(sortingType)) {
+  if (DISABLED_SORT_TYPES.includes(sortType)) {
     isDisabled = 'disabled';
   }
 
   return (
-    `<div class="trip-sort__item  trip-sort__item--${sortingType}">
-        <input id="sort-${sortingType}" class="trip-sort__input  visually-hidden" data-sort-type=${sortingType} type="radio" name="trip-sort" value="sort-${sortingType}" ${isChecked} ${isDisabled}>
-        <label class="trip-sort__btn" for="sort-${sortingType}">${sortingType}</label>
+    `<div class="trip-sort__item  trip-sort__item--${sortType}">
+        <input id="sort-${sortType}" class="trip-sort__input  visually-hidden" data-sort-type=${sortType} type="radio" name="trip-sort" value="sort-${sortType}" ${isChecked} ${isDisabled}>
+        <label class="trip-sort__btn" for="sort-${sortType}">${sortType}</label>
       </div>`
   );
 }
@@ -22,7 +22,7 @@ function createSortItemTemplate (currentSortType, sortingType) {
 function createPointSortingPanelTemplate(currentSortType) {
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-      ${Object.values(SortType).map((sortingType) => createSortItemTemplate(currentSortType, sortingType)).join('')}
+      ${Object.values(SortType).map((sortType) => createSortItemTemplate(currentSortType, sortType)).join('')}
     </form>`
   );
 }
